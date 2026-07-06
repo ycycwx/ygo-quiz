cc9c567b6d1ec0a616047be821873a4c338d6d47
ycycwx <yytcjcy@gmail.com>
2026-07-07 00:35:11 +0800

    fix: prevent extra spaces when copying card names

--- app/columns/Name.tsx
+++ app/columns/Name.tsx
@@ -1,36 +1,25 @@
 import {useCard, useTargetCard} from '../context';
 
-const findIntersection = (name: string, targetName: string) => {
-    const setA = new Set(name.split(''));
-    const setB = new Set(targetName.split(''));
-    return setA.intersection(setB);
-};
-
 export const NameColumn = () => {
     const {name} = useCard();
     const {name: targetName} = useTargetCard();
     if (name === targetName) {
-        return (
-            <div className="flex items-center">
-                <span className="text-green-500">{name}</span>
-            </div>
-        );
+        return <span className="text-green-500">{name}</span>;
     }
 
-    const intersection = findIntersection(name, targetName);
-    return (
-        <div className="flex items-center">
-            {name.split('').map((letter, index) => {
-                if (!intersection.has(letter)) {
-                    return letter;
-                }
+    const targetCharacters = new Set(targetName);
 
-                return (
-                    <span key={index} className="text-green-500">
-                        {letter}
-                    </span>
-                );
-            })}
-        </div>
+    return (
+        <span>
+            {Array.from(name, (character, index) =>
+                targetCharacters.has(character)
+                    ? (
+                        <span key={index} className="text-green-500">
+                            {character}
+                        </span>
+                    )
+                    : character,
+            )}
+        </span>
     );
 };
